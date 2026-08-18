b16d5644c93adbc6179630ba1ea0918937da4c1e
Cosmo <240465688+CosmoWeb@users.noreply.github.com>
2026-08-18 15:14:01 +0300

    Completed the functions in todoItem

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import "./style.css";
 import {todoItem, project} from "./logic.js";
 
-const newTodo = todoItem.addTodo("Study javascript", "18/08/26", "High", "At least one hour");
+const newTodo = todoItem("Study javascript", "18/08/26", "High", "At least one hour");
 
 console.log(newTodo);
+
+newTodo.toggleCheck();
+
+console.log(newTodo);
+
--- src/logic.js
+++ src/logic.js
@@ -1,33 +1,35 @@
-const todoItem = (() => {
-    const addTodo = (title, dueDate, priority, notes) => {
-        title = title;
-        dueDate = dueDate;
-        priority = priority;
-        notes = notes;
-        let check = false;
-
-        return { title, dueDate, priority, notes, check };
-    }; 
-
-    const editTodo = (todoObject) => {
-
+const todoItem = (title, dueDate, priority, notes) => {
+    const property = {
+        title,
+        dueDate,
+        priority,
+        notes,
+        check: false
+    }
+        
+    const editTodo = (newTitle = property.title, newDueDate = property.dueDate, newPriority = property.priority, newNotes = property.notes) => {
+        property.title = newTitle;
+        property.dueDate = newDueDate;
+        property.priority = newPriority;
+        property.notes = newNotes;
     };
 
-    const changePriority = (todoObject, newPriority) => {
-        todoObject.priority = newPriority;
+    const changePriority = (newPriority) => {
+        property.priority = newPriority;
     }
 
-    /*Edit, change priority, set complete */
+    const toggleCheck = () => {
+        property.check = !property.check;
+    };
 
-    return {addTodo};
-})();
+    return {property, editTodo, changePriority, toggleCheck};
+};
 
 const project = (title) =>{
-    const title = title;
+    title = title;
     const storage = [];
 
     return {title, storage};
 };
 
 export { todoItem, project };
-
